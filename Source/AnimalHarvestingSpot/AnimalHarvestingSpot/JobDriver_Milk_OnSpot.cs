ccbf38095a5a8e64c5663a05931791bf66a17c21
emipa606 <emipa606@users.noreply.github.com>
2018-10-31 21:59:36 +0100

    First upload

---
+++ Source/AnimalHarvestingSpot/AnimalHarvestingSpot/JobDriver_Milk_OnSpot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AnimalHarvestingSpot
+{
+	public class JobDriver_Milk_OnSpot: AnimalHarvestingSpot
+    {
+		protected override float WorkTotal
+		{
+			get
+			{
+				return 400f;
+			}
+		}
+		protected override CompHasGatherableBodyResource GetComp(Pawn animal)
+		{
+			return animal.TryGetComp<CompMilkable>();
+		}
+	}
+}
+
---
+++ Source/AnimalHarvestingSpot/AnimalHarvestingSpot/JobDriver_Shear_OnSpot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AnimalHarvestingSpot
+{
+	public class JobDriver_Shear_OnSpot: AnimalHarvestingSpot
+    {
+		protected override float WorkTotal
+		{
+			get
+			{
+				return 1700f;
+			}
+		}
+		protected override CompHasGatherableBodyResource GetComp(Pawn animal)
+		{
+			return animal.TryGetComp<CompShearable>();
+		}
+	}
+}
+
